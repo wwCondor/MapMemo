9528b1c420cf86e84ecf27f370e14f3239916f05
wwCondor <45741282+wwCondor@users.noreply.github.com>
2019-12-14 14:51:36 +0100

    NSFetchedResultsControllerDelegate
    FetchedResultsController now conforms to NSFetchedResultsControllerDelegate to enable updates whenever changes are made tot the tableview.

--- MapMemo/MapMemo/Controller/FetchedResultsController.swift
+++ MapMemo/MapMemo/Controller/FetchedResultsController.swift
@@ -28,3 +28,47 @@ class FetchedResultsController: NSFetchedResultsController<Reminder> {
         }
     }
 }
+
+extension FetchedResultsController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+            // We call this anytime we want insertions, deletions and cell selection to be animated simultaneously
+            // When we use this we also need to sue endUpdates()
+            tableView.beginUpdates()
+        }
+        
+        
+        
+        // This method notifies the receiver that a FetchObject has been changed and indicates the type of the change
+        // Here we have the five arguments; the controller in charge of the change, the object, indexPath of changed object, the type, and the new indexpatch
+        func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+            // The type here is an enum on which we can switch on the type of change that occured
+            switch type {
+            
+                // If we have an insert operation bu no indexPath we cant do anything, so we return
+            case .insert: guard let newIndexPath = newIndexPath else { return }
+            // If we do have an indexPath we can use (In here we can supply an argument for the type of animation we want):
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+                
+            case .delete: guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+
+                
+            case .move, .update: guard let newIndexPath = newIndexPath else { return }
+            tableView.reloadRows(at: [newIndexPath], with: .automatic)
+                
+            @unknown default:
+                return
+            }
+        }
+        
+        
+        
+        // When any changes are made in the contex whether its a save, update, move or delete operation, the context notifies the fetchedResultsController, which then informas its delegate.
+        // Here we implement the relevant delegate method and all we're doing once a change occurs is asking the tableView to reload the data
+        func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    //        tableView.reloadData() // removing this makes tapping the delete button do nothing, we're adding that back in using a different delegate method (see the one above)
+            
+            // With this we inform the tableView that we're done with our updates and it can go ahead and perform animations
+            tableView.endUpdates()
+        }
+}
